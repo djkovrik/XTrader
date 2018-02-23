6c719a1f11808c9ee0e53a5b25e76c989bbdb407
DJ_Kovrik <djkovrik@gmail.com>
2018-02-23 18:43:43 +0300

    Create base fragment for bottom nav  tabs

---
+++ app/src/main/java/com/sedsoftware/wexchanger/presentation/base/BaseTabFragment.kt
@@ -0,0 +1,7 @@
+package com.sedsoftware.wexchanger.presentation.base
+
+import com.sedsoftware.wexchanger.R
+import com.sedsoftware.wexchanger.commons.annotation.LayoutResource
+
+@LayoutResource(R.layout.fragment_tab_container)
+class BaseTabFragment : BaseFragment()
