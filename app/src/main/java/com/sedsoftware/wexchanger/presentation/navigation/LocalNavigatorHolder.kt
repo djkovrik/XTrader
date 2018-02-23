d59a95e7f915b0c4461bc7cf2f80eab996ed53a1
DJ_Kovrik <djkovrik@gmail.com>
2018-02-23 18:43:52 +0300

    Define local navigation holder

---
+++ app/src/main/java/com/sedsoftware/wexchanger/presentation/navigation/LocalNavigatorHolder.kt
@@ -0,0 +1,9 @@
+package com.sedsoftware.wexchanger.presentation.navigation
+
+import ru.terrakok.cicerone.Cicerone
+import ru.terrakok.cicerone.Router
+
+class LocalNavigatorHolder(private val containers: HashMap<String, Cicerone<Router>> = hashMapOf()) {
+
+  fun getCicerone(containerTag: String): Cicerone<Router> = containers.getOrPut(containerTag, { Cicerone.create() })
+}
