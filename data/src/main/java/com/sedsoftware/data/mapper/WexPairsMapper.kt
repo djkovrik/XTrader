49814052065f83c1f650509b914d13248300d227
DJ_Kovrik <djkovrik@gmail.com>
2018-02-24 16:03:40 +0300

    Create currency pair mapper

---
+++ data/src/main/java/com/sedsoftware/data/mapper/WexPairsMapper.kt
@@ -0,0 +1,23 @@
+package com.sedsoftware.data.mapper
+
+import com.sedsoftware.data.entity.WexCurrencyPairsList
+import com.sedsoftware.domain.entity.CurrencyPair
+import javax.inject.Inject
+
+class WexPairsMapper @Inject constructor() {
+
+  fun map(from: WexCurrencyPairsList): List<CurrencyPair> =
+    from.pairs.map { (pairName, pairInfo) ->
+      CurrencyPair(
+        name = pairName,
+        first = pairName.substringBefore("_"),
+        second = pairName.substringAfter("_"),
+        decimalPlaces = pairInfo.decimalPlaces,
+        minPrice = pairInfo.minPrice,
+        maxPrice = pairInfo.maxPrice,
+        minAmount = pairInfo.minAmount,
+        hidden = pairInfo.hidden,
+        fee = pairInfo.fee
+      )
+    }
+}
