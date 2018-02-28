c8584e5478f99cb36affdbb1f5e2a4dccdc634d2
DJ_Kovrik <djkovrik@gmail.com>
2018-02-28 21:52:11 +0300

    Implement home screen bottom navigation view

--- app/src/main/java/com/sedsoftware/wexchanger/presentation/features/home/MainActivity.kt
+++ app/src/main/java/com/sedsoftware/wexchanger/presentation/features/home/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.sedsoftware.wexchanger.presentation.features.home
 
+import android.os.Bundle
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
+import com.mikepenz.community_material_typeface_library.CommunityMaterial
 import com.sedsoftware.wexchanger.R
 import com.sedsoftware.wexchanger.commons.annotation.LayoutResource
+import com.sedsoftware.wexchanger.commons.extension.color
+import com.sedsoftware.wexchanger.commons.extension.iconics
+import com.sedsoftware.wexchanger.commons.extension.string
 import com.sedsoftware.wexchanger.di.AppScope
 import com.sedsoftware.wexchanger.presentation.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.home_bottom_navigation
 import toothpick.Toothpick
 
 @LayoutResource(R.layout.activity_main)
@@ -17,6 +25,39 @@ class MainActivity : BaseActivity(), MainActivityView {
   @ProvidePresenter
   fun providePresenter(): MainActivityPresenter =
     Toothpick
-      .openScope(AppScope.NETWORK)
+      .openScope(AppScope.DATASOURCE)
       .getInstance(MainActivityPresenter::class.java)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    initBottomNavigation()
+  }
+
+  private fun initBottomNavigation() {
+    val marketTabItem =
+        AHBottomNavigationItem(string(R.string.tab_market), iconics(CommunityMaterial.Icon.cmd_home))
+
+    val ordersTabItem =
+        AHBottomNavigationItem(string(R.string.tab_orders), iconics(CommunityMaterial.Icon.cmd_currency_btc))
+
+    val walletTabItem =
+        AHBottomNavigationItem(string(R.string.tab_wallet), iconics(CommunityMaterial.Icon.cmd_wallet))
+
+    val trackerTabItem =
+        AHBottomNavigationItem(string(R.string.tab_tracker), iconics(CommunityMaterial.Icon.cmd_radar))
+
+    val settingsTabItem =
+        AHBottomNavigationItem(string(R.string.tab_settings), iconics(CommunityMaterial.Icon.cmd_settings))
+
+    home_bottom_navigation.addItems(
+      listOf(marketTabItem, ordersTabItem, walletTabItem, trackerTabItem, settingsTabItem)
+    )
+
+    home_bottom_navigation.isBehaviorTranslationEnabled = true
+    home_bottom_navigation.defaultBackgroundColor = color(R.color.colorPrimary)
+    home_bottom_navigation.accentColor = color(R.color.colorTabIcon)
+    home_bottom_navigation.inactiveColor = color(R.color.colorTabIconInactive)
+    home_bottom_navigation.titleState = AHBottomNavigation.TitleState.SHOW_WHEN_ACTIVE
+  }
 }
--- app/src/main/java/com/sedsoftware/wexchanger/presentation/features/home/MainActivityPresenter.kt
+++ app/src/main/java/com/sedsoftware/wexchanger/presentation/features/home/MainActivityPresenter.kt
@@ -1,26 +1,8 @@
 package com.sedsoftware.wexchanger.presentation.features.home
 
 import com.arellomobile.mvp.InjectViewState
-import com.sedsoftware.domain.interactor.GetCurrencyPairsUseCase
-import com.sedsoftware.wexchanger.commons.extension.addToCancelableJobsPool
 import com.sedsoftware.wexchanger.presentation.base.BasePresenter
-import timber.log.Timber
 import javax.inject.Inject
 
 @InjectViewState
-class MainActivityPresenter @Inject constructor(
-  private val getCurrencyPairsUseCase: GetCurrencyPairsUseCase
-) : BasePresenter<MainActivityView>() {
-
-  override fun attachView(view: MainActivityView?) {
-    super.attachView(view)
-
-    getCurrencyPairsUseCase
-      .getPairs({ list ->
-        Timber.d("Pairs: $list") }, {
-          throwable ->
-        Timber.e("Error: ${throwable.message}")
-      })
-      .addToCancelableJobsPool(jobs)
-  }
-}
+class MainActivityPresenter @Inject constructor() : BasePresenter<MainActivityView>()
