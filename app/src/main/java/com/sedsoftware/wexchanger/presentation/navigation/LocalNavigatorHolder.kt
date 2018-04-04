77bf65fa1e70fdaee4c4257f697e41dbbc7b058b
DJ_Kovrik <djkovrik@gmail.com>
2018-04-04 17:18:17 +0300

    GradleGitVersioning config

--- app/src/main/java/com/sedsoftware/wexchanger/presentation/navigation/LocalNavigatorHolder.kt
+++ app/src/main/java/com/sedsoftware/wexchanger/presentation/navigation/LocalNavigatorHolder.kt
@@ -3,7 +3,7 @@ package com.sedsoftware.wexchanger.presentation.navigation
 import ru.terrakok.cicerone.Cicerone
 import ru.terrakok.cicerone.Router
 
-class LocalNavigatorHolder(private val containers: HashMap<String, Cicerone<Router>> = hashMapOf()) {
+class LocalNavigatorHolder(private val containers: MutableMap<String, Cicerone<Router>> = hashMapOf()) {
 
   fun getCicerone(containerTag: String): Cicerone<Router> =
     containers.getOrPut(containerTag, { Cicerone.create() })
