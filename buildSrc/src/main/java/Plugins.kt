b4e16f77f9029d2916c990c6c7f3bdb12c16df46
DJ_Kovrik <djkovrik@gmail.com>
2018-05-16 22:23:35 +0300

    Additional detekt config

--- buildSrc/src/main/java/Plugins.kt
+++ buildSrc/src/main/java/Plugins.kt
@@ -6,9 +6,14 @@ import Versions.GRADLE_VERSIONS_VERSION
 import Versions.KOTLIN_VERSION
 
 object Plugins {
-  const val ANDROID_GRADLE = "com.android.tools.build:gradle:$GRADLE_ANDROID_VERSION"
-  const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
-  const val DETEKT = "gradle.plugin.io.gitlab.arturbosch.detekt:detekt-gradle-plugin:$DETEKT_VERSION"
-  const val GRADLE_VERSIONS = "com.github.ben-manes:gradle-versions-plugin:$GRADLE_VERSIONS_VERSION"
-  const val GRADLE_VERSIONING = "gradle.plugin.com.gladed.gradle.androidgitversion:gradle-android-git-version:$GRADLE_VERSIONING_VERSION"
+  const val ANDROID_GRADLE =
+    "com.android.tools.build:gradle:$GRADLE_ANDROID_VERSION"
+  const val KOTLIN_GRADLE_PLUGIN =
+    "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VERSION"
+  const val DETEKT =
+    "gradle.plugin.io.gitlab.arturbosch.detekt:detekt-gradle-plugin:$DETEKT_VERSION"
+  const val GRADLE_VERSIONS =
+    "com.github.ben-manes:gradle-versions-plugin:$GRADLE_VERSIONS_VERSION"
+  const val GRADLE_VERSIONING =
+    "gradle.plugin.com.gladed.gradle.androidgitversion:gradle-android-git-version:$GRADLE_VERSIONING_VERSION"
 }
