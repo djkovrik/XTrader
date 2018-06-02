2b588aea2a1c2d76e2ad34cc7f965952ebaaedb9
DJ_Kovrik <djkovrik@gmail.com>
2018-06-02 14:55:08 +0300

    Provide settings

--- sources/base/device/src/main/java/com/sedsoftware/device/di/DeviceToolsModule.kt
+++ sources/base/device/src/main/java/com/sedsoftware/device/di/DeviceToolsModule.kt
@@ -1,9 +1,15 @@
 package com.sedsoftware.device.di
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Resources
+import android.preference.PreferenceManager
 import com.sedsoftware.core.device.Logger
+import com.sedsoftware.core.device.Settings
 import com.sedsoftware.core.device.Signer
 import com.sedsoftware.device.encrypt.SignerImpl
 import com.sedsoftware.device.log.LoggerImpl
+import com.sedsoftware.device.settings.SettingsImpl
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -11,17 +17,28 @@ import javax.inject.Singleton
 @Module
 class DeviceToolsModule {
 
-  @Module
-  companion object {
+  @Provides
+  @Singleton
+  fun provideLogger(): Logger =
+    LoggerImpl()
 
-    @JvmStatic
-    @Provides
-    @Singleton
-    fun provideLogger(): Logger = LoggerImpl()
+  @Provides
+  @Singleton
+  fun provideSigner(): Signer =
+    SignerImpl()
 
-    @JvmStatic
-    @Provides
-    @Singleton
-    fun provideSigner(): Signer = SignerImpl()
-  }
+  @Provides
+  @Singleton
+  fun provideResources(context: Context): Resources =
+    context.resources
+
+  @Provides
+  @Singleton
+  fun provideSharedPreferences(context: Context): SharedPreferences =
+    PreferenceManager.getDefaultSharedPreferences(context)
+
+  @Provides
+  @Singleton
+  fun provideSettings(resources: Resources, preferences: SharedPreferences): Settings =
+    SettingsImpl(resources, preferences)
 }
