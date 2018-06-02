90c17bbdd637bbee8002c308647e8fb62b5585c7
DJ_Kovrik <djkovrik@gmail.com>
2018-06-02 11:17:06 +0300

    Add basic base classes

--- sources/features/navigation/src/main/java/com/sedsoftware/navigation/MainActivity.kt
+++ sources/features/navigation/src/main/java/com/sedsoftware/navigation/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.sedsoftware.navigation
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import com.sedsoftware.uicore.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+  override val layoutId: Int
+    get() = R.layout.activity_main
 }
