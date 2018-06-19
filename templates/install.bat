f1e6d4b3e95d6ee8fbf8db49393d8a9eba211fee
DJ_Kovrik <djkovrik@gmail.com>
2018-06-19 22:19:18 +0300

    Basic templates install script

--- templates/install.bat
+++ templates/install.bat
@@ -1,1 +1,7 @@
-SET AndroidStudioPath = "D:\Android\Android Studio Canary"
+SET ANDROID_STUDIO_PATH=D:\Android\Android Studio Canary
+SET DESTINATION=%ANDROID_STUDIO_PATH%\plugins\android\lib\templates\other\Modular\Injector\
+
+xcopy /s /y "injector" "%DESTINATION%"
+
+echo "Installation completed."
+pause
