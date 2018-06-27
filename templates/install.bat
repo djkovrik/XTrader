4a1597853a13bdc7e9ea1bbd3abacf8f17e2235a
DJ_Kovrik <djkovrik@gmail.com>
2018-06-27 21:41:03 +0300

    Add core to install script

--- templates/install.bat
+++ templates/install.bat
@@ -1,7 +1,9 @@
 SET ANDROID_STUDIO_PATH=D:\Android\Android Studio Canary
 
+SET CORE_DESTINATION=%ANDROID_STUDIO_PATH%\plugins\android\lib\templates\other\ModularCore\
 SET INJECTOR_DESTINATION=%ANDROID_STUDIO_PATH%\plugins\android\lib\templates\other\ModularInjector\
 
+xcopy /s /y "injector" "%CORE_DESTINATION%"
 xcopy /s /y "injector" "%INJECTOR_DESTINATION%"
 
 echo "Installation completed."
