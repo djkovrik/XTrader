6b7e605cf434404434988e03bf068b94d6bd6480
DJ_Kovrik <djkovrik@gmail.com>
2018-08-04 17:30:51 +0300

    Define base enums

---
+++ sources/base/core/src/main/java/com/sedsoftware/core/common/enum/DataSource.kt
@@ -0,0 +1,7 @@
+package com.sedsoftware.core.common.enum
+
+enum class DataSource {
+    ANY,
+    LOCAL,
+    REMOTE
+}
---
+++ sources/base/core/src/main/java/com/sedsoftware/core/common/enum/Exchange.kt
@@ -0,0 +1,17 @@
+package com.sedsoftware.core.common.enum
+
+enum class Exchange {
+    BINANCE,
+    BITFINEX,
+    BITTREX,
+    CEXIO,
+    COINBASEPRO,
+    COINEX,
+    HUOBI,
+    HITBTC,
+    OKEX,
+    POLONIEX,
+    KRAKEN,
+    YOBIT,
+    WEX,
+}
