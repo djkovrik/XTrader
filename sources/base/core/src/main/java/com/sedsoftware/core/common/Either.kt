8d222ffd1e0bc5fcd17822701effd73e08ed2203
DJ_Kovrik <djkovrik@gmail.com>
2018-08-09 21:52:00 +0300

    Rename Either parts

--- sources/base/core/src/main/java/com/sedsoftware/core/common/Either.kt
+++ sources/base/core/src/main/java/com/sedsoftware/core/common/Either.kt
@@ -1,19 +1,19 @@
 package com.sedsoftware.core.common
 
 sealed class Either<out L, out R> {
-    data class Left<out L>(val a: L) : Either<L, Nothing>()
-    data class Right<out R>(val b: R) : Either<Nothing, R>()
+    data class Error<out L>(val a: L) : Either<L, Nothing>()
+    data class Success<out R>(val b: R) : Either<Nothing, R>()
 
-    val isRight get() = this is Right<R>
-    val isLeft get() = this is Left<L>
+    val isSuccess get() = this is Success<R>
+    val isError get() = this is Error<L>
 
-    fun <L> left(a: L) = Either.Left(a)
-    fun <R> right(b: R) = Either.Right(b)
+    fun <L> error(a: L) = Either.Error(a)
+    fun <R> success(b: R) = Either.Success(b)
 
     fun either(fnL: (L) -> Any, fnR: (R) -> Any): Any =
         when (this) {
-            is Left -> fnL(a)
-            is Right -> fnR(b)
+            is Error -> fnL(a)
+            is Success -> fnR(b)
         }
 }
 
@@ -23,8 +23,8 @@ fun <A, B, C> ((A) -> B).c(f: (B) -> C): (A) -> C = {
 
 fun <T, L, R> Either<L, R>.flatMap(fn: (R) -> Either<L, T>): Either<L, T> =
     when (this) {
-        is Either.Left -> Either.Left(a)
-        is Either.Right -> fn(b)
+        is Either.Error -> Either.Error(a)
+        is Either.Success -> fn(b)
     }
 
-fun <T, L, R> Either<L, R>.map(fn: (R) -> (T)): Either<L, T> = this.flatMap(fn.c(::right))
+fun <T, L, R> Either<L, R>.map(fn: (R) -> (T)): Either<L, T> = this.flatMap(fn.c(::success))
