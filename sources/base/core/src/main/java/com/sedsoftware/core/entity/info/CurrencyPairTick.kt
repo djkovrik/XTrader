99e6f18995ab44b4061c493257bbc18f90570f88
DJ_Kovrik <djkovrik@gmail.com>
2018-08-18 17:31:09 +0300

    Ticks mapper

--- sources/base/core/src/main/java/com/sedsoftware/core/entity/info/CurrencyPairTick.kt
+++ sources/base/core/src/main/java/com/sedsoftware/core/entity/info/CurrencyPairTick.kt
@@ -1,12 +1,12 @@
 package com.sedsoftware.core.entity.info
 
-import com.sedsoftware.core.entity.CurrencyPair
 import com.sedsoftware.core.entity.Exchange
 
 interface CurrencyPairTick {
-    val pair: CurrencyPair
     val exchange: Exchange
-    val bid: Float
-    val ask: Float
-    val last: Float
+    val symbol: String
+    val bidPrice: Float
+    val bidQuantity: Float
+    val askPrice: Float
+    val askQuantity: Float
 }
--- sources/exchanges/ex_binance/src/main/java/com/sedsoftware/binance/entity/BinanceCurrencyPairTick.kt
+++ sources/exchanges/ex_binance/src/main/java/com/sedsoftware/binance/entity/BinanceCurrencyPairTick.kt
@@ -1,13 +1,13 @@
 package com.sedsoftware.binance.entity
 
-import com.sedsoftware.core.entity.CurrencyPair
 import com.sedsoftware.core.entity.Exchange
 import com.sedsoftware.core.entity.info.CurrencyPairTick
 
 data class BinanceCurrencyPairTick(
-    override val pair: CurrencyPair,
     override val exchange: Exchange,
-    override val bid: Float,
-    override val ask: Float,
-    override val last: Float
+    override val symbol: String,
+    override val bidPrice: Float,
+    override val bidQuantity: Float,
+    override val askPrice: Float,
+    override val askQuantity: Float
 ) : CurrencyPairTick
