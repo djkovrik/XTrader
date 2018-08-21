a3fd159f635600ff5d9591105b3f644af5e40b93
DJ_Kovrik <djkovrik@gmail.com>
2018-08-21 21:08:33 +0300

    Binance tick mapper test

---
+++ sources/exchanges/ex_binance/src/test/java/com/sedsoftware/binance/mapper/BinanceTicksMapperTest.kt
@@ -0,0 +1,49 @@
+package com.sedsoftware.binance.mapper
+
+import com.sedsoftware.binance.database.model.BinanceTickDbModel
+import com.sedsoftware.binance.entity.BinanceCurrencyPairTick
+import com.sedsoftware.binance.fakedata.FakeTickData
+import com.sedsoftware.binance.network.model.SymbolTickModel
+import com.winterbe.expekt.should
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.lifecycle.CachingMode
+import org.spekframework.spek2.style.specification.describe
+
+class BinanceTicksMapperTest : Spek({
+
+    describe("Symbol tick mapper") {
+
+        val mapper by memoized { BinanceTicksMapper() }
+        val fakeDataSource by memoized(CachingMode.SCOPE) { FakeTickData() }
+
+        lateinit var parsedEntity: SymbolTickModel
+
+        context("Parsing json from cloud") {
+            parsedEntity = fakeDataSource.getRawParsedData()
+
+            it("Should return correct entity") {
+                parsedEntity.should.equal(fakeDataSource.getPredefinedParsedEntity())
+            }
+        }
+
+        lateinit var mappedDbEntity: BinanceTickDbModel
+
+        context("Mapping from parsed cloud to db") {
+            mappedDbEntity = mapper.mapFromCloudToDb(parsedEntity)
+
+            it("should return correct value") {
+                mappedDbEntity.should.equal(fakeDataSource.getDatabasEntity())
+            }
+        }
+
+        lateinit var mappedDomainEntity: BinanceCurrencyPairTick
+
+        context("Mapping from db to entity") {
+            mappedDomainEntity = mapper.mapFromDbToEntity(mappedDbEntity)
+
+            it("should return correct value") {
+                mappedDomainEntity.should.equal(fakeDataSource.getDomainEntity())
+            }
+        }
+    }
+})
