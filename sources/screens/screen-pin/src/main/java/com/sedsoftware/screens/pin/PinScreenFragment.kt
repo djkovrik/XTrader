cc899ea7968d23e939f69987b77524125bc98663
DJ_Kovrik <djkovrik@gmail.com>
2018-10-04 20:12:09 +0300

    Layout fix

--- sources/screens/screen-pin/src/main/java/com/sedsoftware/screens/pin/PinScreenFragment.kt
+++ sources/screens/screen-pin/src/main/java/com/sedsoftware/screens/pin/PinScreenFragment.kt
@@ -1,14 +1,10 @@
 package com.sedsoftware.screens.pin
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.sedsoftware.core.presentation.base.BaseFragment
 
 class PinScreenFragment : BaseFragment() {
 
-    override fun getLayoutId(): Int = R.layout.fragment_pin
+    override fun getLayoutId(): Int = R.layout.fragment_pin_screen
 
     override fun inject() {
     }
