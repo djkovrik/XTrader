9a1b792ab98869c4a5716e67fb78687791677480
DJ_Kovrik <djkovrik@gmail.com>
2018-10-14 18:42:01 +0300

    Basic intro layout

--- sources/screens/screen-intro/src/main/java/com/sedsoftware/screens/intro/IntroScreenFragment.kt
+++ sources/screens/screen-intro/src/main/java/com/sedsoftware/screens/intro/IntroScreenFragment.kt
@@ -1,10 +1,18 @@
 package com.sedsoftware.screens.intro
 
+import android.os.Bundle
+import android.view.View
 import com.sedsoftware.core.presentation.base.BaseFragment
+import com.sedsoftware.core.presentation.extension.setBackgroundColor
 
 class IntroScreenFragment : BaseFragment() {
 
     override fun getLayoutId(): Int = R.layout.fragment_intro_screen
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBackgroundColor(R.attr.colorPrimaryDark)
+    }
 
     override fun inject() {
     }
