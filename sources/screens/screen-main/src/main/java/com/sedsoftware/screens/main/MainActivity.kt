1d22ded78c5f80611d8aa26c943bc3ec48a540e0
DJ_Kovrik <djkovrik@gmail.com>
2019-01-06 17:01:31 +0300

    Setup navController holder

--- sources/screens/screen-main/src/main/java/com/sedsoftware/screens/main/MainActivity.kt
+++ sources/screens/screen-main/src/main/java/com/sedsoftware/screens/main/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.sedsoftware.screens.main
 
+import androidx.navigation.Navigation
 import com.sedsoftware.core.di.holder.ActivityComponentHolder
 import com.sedsoftware.core.di.provider.MainActivityToolsProvider
+import com.sedsoftware.core.navigation.NavControllerHolder
 import com.sedsoftware.core.presentation.base.BaseActivity
 import com.sedsoftware.screens.main.di.MainActivityComponent
+import javax.inject.Inject
 
 class MainActivity : BaseActivity(), ActivityComponentHolder {
+
+    @Inject
+    lateinit var navControllerHolder: NavControllerHolder
 
     private val mainActivityComponent: MainActivityToolsProvider by lazy {
         MainActivityComponent.Initializer.init(appComponent)
@@ -20,4 +26,16 @@ class MainActivity : BaseActivity(), ActivityComponentHolder {
 
     override fun getActivityComponent(): MainActivityToolsProvider =
         mainActivityComponent
+
+    override fun onPause() {
+        navControllerHolder.removeNavController()
+        super.onPause()
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        Navigation.findNavController(this, R.id.nav_controller_main).let { controller ->
+            navControllerHolder.setNavController(controller)
+        }
+    }
 }
