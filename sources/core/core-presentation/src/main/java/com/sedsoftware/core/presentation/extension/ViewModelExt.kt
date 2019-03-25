14161f6e85d58f01705b89ba6a04360f5659f939
DJ_Kovrik <djkovrik@gmail.com>
2019-03-25 19:23:55 +0300

    Fix viewmodel extension

--- sources/core/core-presentation/src/main/java/com/sedsoftware/core/presentation/extension/ViewModelExt.kt
+++ sources/core/core-presentation/src/main/java/com/sedsoftware/core/presentation/extension/ViewModelExt.kt
@@ -2,12 +2,13 @@ package com.sedsoftware.core.presentation.extension
 
 import androidx.lifecycle.viewModelScope
 import com.sedsoftware.core.presentation.base.BaseViewModel
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
-fun BaseViewModel.launch(block: () -> Unit) =
-    viewModelScope.launch { block.invoke() }
+fun BaseViewModel.launch(block: suspend CoroutineScope.() -> Unit) =
+    viewModelScope.launch { block.invoke(this) }
 
-fun <T> BaseViewModel.async(block: () -> T): Deferred<T> =
-    viewModelScope.async { block.invoke() }
+fun <T> BaseViewModel.async(block: suspend CoroutineScope.() -> T): Deferred<T> =
+    viewModelScope.async { block.invoke(this) }
