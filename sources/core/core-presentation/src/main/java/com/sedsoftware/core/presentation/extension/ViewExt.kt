068a9c7d3d7ae4987eecabaa1310a47aec316f7d
DJ_Kovrik <djkovrik@gmail.com>
2019-04-04 20:21:30 +0300

    Visibility ext funcs

---
+++ sources/core/core-presentation/src/main/java/com/sedsoftware/core/presentation/extension/ViewExt.kt
@@ -0,0 +1,7 @@
+package com.sedsoftware.core.presentation.extension
+
+import android.view.View
+
+fun View.show() = { visibility = View.VISIBLE }
+fun View.hide() = { visibility = View.INVISIBLE }
+fun View.gone() = { visibility = View.GONE }
