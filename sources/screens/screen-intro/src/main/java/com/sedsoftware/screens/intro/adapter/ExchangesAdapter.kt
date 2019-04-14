173154fc87a63bf9a7acb3d0ae6c3ba91c46d294
DJ_Kovrik <djkovrik@gmail.com>
2019-04-14 19:27:13 +0300

    Animate adapter items

--- sources/screens/screen-intro/src/main/java/com/sedsoftware/screens/intro/adapter/ExchangesAdapter.kt
+++ sources/screens/screen-intro/src/main/java/com/sedsoftware/screens/intro/adapter/ExchangesAdapter.kt
@@ -2,22 +2,25 @@ package com.sedsoftware.screens.intro.adapter
 
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.sedsoftware.core.domain.entity.Exchange
 import com.sedsoftware.core.domain.provider.AssetsProvider
+import com.sedsoftware.core.presentation.custom.DownloadState
 import com.sedsoftware.core.presentation.extension.dim
 import com.sedsoftware.core.presentation.extension.inflate
-import com.sedsoftware.core.presentation.custom.DownloadState
 import com.sedsoftware.screens.intro.R
 import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.fragment_intro_screen_item.*
+import kotlinx.android.synthetic.main.fragment_intro_screen_item.intro_button_download
+import kotlinx.android.synthetic.main.fragment_intro_screen_item.intro_exchange_logo
+import kotlinx.android.synthetic.main.fragment_intro_screen_item.intro_exchange_name
 import javax.inject.Inject
 import kotlin.properties.Delegates
 
 class ExchangesAdapter @Inject constructor(
     private val assetsProvider: AssetsProvider
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<ViewHolder>() {
 
     internal var items: Map<Exchange, DownloadState> by Delegates.observable(emptyMap()) { _, _, newValue ->
 
@@ -39,12 +42,20 @@ class ExchangesAdapter @Inject constructor(
     private val exchanges = mutableListOf<Exchange>()
     private val states = mutableListOf<DownloadState>()
 
+    private var lastPosition = -1
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ExchangeItemViewHolder(parent)
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder as ExchangeItemViewHolder
         holder.bind(exchanges[position], states[position], assetsProvider, clickListener)
+        setAnimation(holder.itemView, position)
+    }
+
+    override fun onViewDetachedFromWindow(holder: ViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        holder.itemView.clearAnimation()
     }
 
     override fun setHasStableIds(hasStableIds: Boolean) {
@@ -57,8 +68,20 @@ class ExchangesAdapter @Inject constructor(
     override fun getItemId(position: Int): Long =
         position.toLong()
 
+    private fun setAnimation(itemView: View, position: Int) {
+        if (position > lastPosition) {
+
+            AnimationUtils.loadAnimation(itemView.context, R.anim.recyclerview_item_appear)
+                .apply {
+                    itemView.startAnimation(this)
+                }
+
+            lastPosition = position
+        }
+    }
+
     class ExchangeItemViewHolder(parent: ViewGroup) :
-        RecyclerView.ViewHolder(parent.inflate(R.layout.fragment_intro_screen_item)), LayoutContainer {
+        ViewHolder(parent.inflate(R.layout.fragment_intro_screen_item)), LayoutContainer {
 
         override val containerView: View?
             get() = itemView
