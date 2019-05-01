730954516ac6ae4e1cd6235287ed23df5ba88e22
DJ_Kovrik <djkovrik@gmail.com>
2019-05-01 12:41:40 +0300

    Remove base activity

--- sources/screens/screen-main/src/main/java/com/sedsoftware/screens/main/MainActivity.kt
+++ sources/screens/screen-main/src/main/java/com/sedsoftware/screens/main/MainActivity.kt
@@ -1,35 +1,32 @@
 package com.sedsoftware.screens.main
 
-import android.view.ViewGroup
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
+import com.sedsoftware.core.di.App
 import com.sedsoftware.core.di.holder.ActivityToolsHolder
 import com.sedsoftware.core.di.provider.MainActivityToolsProvider
 import com.sedsoftware.core.navigation.NavControllerHolder
-import com.sedsoftware.core.presentation.base.BaseActivity
+import com.sedsoftware.core.presentation.extension.setBackgroundColor
 import com.sedsoftware.screens.main.di.MainActivityComponent
-import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-class MainActivity : BaseActivity(), ActivityToolsHolder {
+class MainActivity : AppCompatActivity(), ActivityToolsHolder {
+
+    private val mainActivityComponent: MainActivityComponent by lazy {
+        val appComponent = (applicationContext as App).getAppComponent()
+        MainActivityComponent.Initializer.init(appComponent)
+    }
 
     @Inject
     lateinit var navControllerHolder: NavControllerHolder
 
-    private val mainActivityComponent: MainActivityComponent by lazy {
-        MainActivityComponent.Initializer.init(appComponent)
-    }
-
-    override fun getLayoutResId(): Int = R.layout.activity_main
-
-    override fun inject() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         mainActivityComponent.inject(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setBackgroundColor(R.color.colorBackground)
     }
-
-    override fun getActivityToolsProvider(): MainActivityToolsProvider =
-        mainActivityComponent
-
-    override fun getRootContainer(): ViewGroup =
-        root_container
 
     override fun onPause() {
         navControllerHolder.removeNavController()
@@ -38,7 +35,9 @@ class MainActivity : BaseActivity(), ActivityToolsHolder {
 
     override fun onResumeFragments() {
         super.onResumeFragments()
-        val controller = Navigation.findNavController(this, R.id.nav_controller_main)
-        navControllerHolder.setNavController(controller)
+        navControllerHolder.setNavController(Navigation.findNavController(this, R.id.nav_controller_main))
     }
+
+    override fun getActivityToolsProvider(): MainActivityToolsProvider =
+        mainActivityComponent
 }
