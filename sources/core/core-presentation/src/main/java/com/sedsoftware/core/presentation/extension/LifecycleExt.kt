8c9fea37960071b50bf01105e19e36f899bc518a
DJ_Kovrik <djkovrik@gmail.com>
2019-06-01 20:21:53 +0300

    New lifecycle deps

--- sources/core/core-presentation/src/main/java/com/sedsoftware/core/presentation/extension/LifecycleExt.kt
+++ sources/core/core-presentation/src/main/java/com/sedsoftware/core/presentation/extension/LifecycleExt.kt
@@ -3,10 +3,21 @@ package com.sedsoftware.core.presentation.extension
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.sedsoftware.core.utils.common.Failure
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 fun <T : Any, L : LiveData<T>> LifecycleOwner.observe(liveData: L, body: (T?) -> Unit) =
     liveData.observe(this, Observer(body))
 
 fun <L : LiveData<Failure>> LifecycleOwner.failure(liveData: L, body: (Failure?) -> Unit) =
     liveData.observe(this, Observer(body))
+
+fun LifecycleOwner.launch(block: suspend CoroutineScope.() -> Unit) =
+    lifecycleScope.launch { block.invoke(this) }
+
+fun <T> LifecycleOwner.async(block: suspend CoroutineScope.() -> T): Deferred<T> =
+    lifecycleScope.async { block.invoke(this) }
