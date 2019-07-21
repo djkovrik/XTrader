1bab921784d0fcfdd2f6451ed156d7767ee472c2
DJ_Kovrik <djkovrik@gmail.com>
2019-07-21 21:59:45 +0300

    Add shapeOfView

--- buildSrc/src/main/java/UiDependencies.kt
+++ buildSrc/src/main/java/UiDependencies.kt
@@ -1,10 +1,12 @@
 import Versions.MATERIAL_DIALOGS_VERSION
 import Versions.MATERIAL_VALUES_VERSION
 import Versions.MP_ANDROID_CHART_VERSION
+import Versions.SHAPE_OF_VIEW_VERSION
 
 object UiDependencies {
     const val materialValues = "blue.aodev:material-values:$MATERIAL_VALUES_VERSION"
     const val materialDialogs = "com.afollestad.material-dialogs:core:$MATERIAL_DIALOGS_VERSION"
     const val materialDialogsCommons = "com.afollestad.material-dialogs:commons:$MATERIAL_DIALOGS_VERSION"
     const val mpAndroidChart = "com.github.philjay:mpandroidchart:$MP_ANDROID_CHART_VERSION"
+    const val shapeOfView = "com.github.florent37:shapeofview:$SHAPE_OF_VIEW_VERSION"
 }
