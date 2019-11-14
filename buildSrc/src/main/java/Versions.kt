361f11f5df7165c0853e3fe87f02c635591641ca
DJ_Kovrik <djkovrik@gmail.com>
2019-11-14 21:06:42 +0300

    Fix spek

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -8,13 +8,13 @@ object Versions {
     const val VERSION_NAME = "0.0.1"
 
     // Firebase
-    const val PLAY_SERVICES_VERSION = "4.3.2"
+    const val PLAY_SERVICES_VERSION = "4.3.3"
     const val FIREBASE_CORE_VERSION = "17.2.1"
     const val FIREBASE_ANALYTICS_VERSION = "17.2.1"
     const val FIREBASE_FIRESTORE_VERSION = "21.0.0"
     const val FIREBASE_CLOUD_MESSAGING_VERSION = "20.0.0"
     const val FIREBASE_CRASHLYTICS_VERSION = "2.10.1"
-    const val FIREBASE_PERFORMANCE_MONITOR_VERSION = "19.0.1"
+    const val FIREBASE_PERFORMANCE_MONITOR_VERSION = "19.0.2"
 
     // Core
     const val ADAPTER_DELEGATES_VERSION = "4.2.0"
@@ -32,7 +32,7 @@ object Versions {
 
     // Plugins
     const val GRADLE_ANDROID_VERSION = "3.4.0"
-    const val FABRIC_VERSION = "1.31.0"
+    const val FABRIC_VERSION = "1.31.2"
     const val DETEKT_VERSION = "1.1.1"
     const val GRADLE_VERSIONS_VERSION = "0.27.0"
     const val ANDROID_JUNIT5_VERSION = "1.5.2.0"
@@ -45,11 +45,11 @@ object Versions {
     const val LEGACY_SUPPORT_UTILS_VERSION = "1.0.0"
 
     // JetPack - Architecture
-    const val LIFECYCLE_VERSION = "2.2.0-rc01"
+    const val LIFECYCLE_VERSION = "2.2.0-rc02"
     const val ROOM_VERSION = "2.2.1"
 
     // JetPack - KTX
-    const val KTX_VERSION = "1.2.0-beta01"
+    const val KTX_VERSION = "1.2.0-beta02"
 
     // UI
     const val MATERIAL_VALUES_VERSION = "1.1.1"
@@ -63,7 +63,7 @@ object Versions {
     const val TEST_RUNNER_VERSION = "1.3.0-alpha02"
     const val TEST_RULES_VERSION = "1.1.0-alpha1"
     const val ESPRESSO_VERSION = "3.1.0-alpha1"
-    const val SPEK_VERSION = "2.0.8"
+    const val SPEK_VERSION = "2.0.7"
     const val EXPEKT_VERSION = "0.5.0"
     const val KAKAO_VERSION = "1.3.0"
     const val FUEL_VERSION = "2.2.1"
--- sources/core/core-test/src/main/java/com/sedsoftware/core/test/FuelExt.kt
+++ sources/core/core-test/src/main/java/com/sedsoftware/core/test/FuelExt.kt
@@ -2,11 +2,13 @@ package com.sedsoftware.core.test
 
 import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 fun <T : Any> moshiDeserializerOf(clazz: Class<T>, adapter: Any) = object : ResponseDeserializable<T> {
 
     override fun deserialize(content: String): T? = Moshi.Builder()
         .add(adapter)
+        .add(KotlinJsonAdapterFactory())
         .build()
         .adapter(clazz)
         .fromJson(content)
