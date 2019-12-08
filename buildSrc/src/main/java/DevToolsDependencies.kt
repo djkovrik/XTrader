a3e3ac072946287726617e82c31aabdb4fe3ba0d
DJ_Kovrik <djkovrik@gmail.com>
2019-12-08 21:48:53 +0300

    Remove buildSrc

--- buildSrc/src/main/java/DevToolsDependencies.kt
+++
@@ -1,9 +0,0 @@
-import Versions.ANDROID_DEBUG_DATABASE_VERSION
-import Versions.LEAK_CANARY_VERSION
-import Versions.TIMBER_VERSION
-
-object DevToolsDependencies {
-    const val timber = "com.jakewharton.timber:timber:$TIMBER_VERSION"
-    const val leakCanary = "com.squareup.leakcanary:leakcanary-android:$LEAK_CANARY_VERSION"
-    const val androidDebugDatabase = "com.amitshekhar.android:debug-db:$ANDROID_DEBUG_DATABASE_VERSION"
-}
