ad90e6199a62c1f6756bf6df2e6ebaf6e3e2ffad
DJ_Kovrik <djkovrik@gmail.com>
2020-02-13 21:25:00 +0300

    Basic components manager

---
+++ sources/core/core-di/src/main/java/com/sedsoftware/core/di/management/HasDaggerComponent.kt
@@ -0,0 +1,23 @@
+package com.sedsoftware.core.di.management
+
+/**
+ * Highlights that class which implements this interface can create Dagger 2 component
+ *
+ * @param <T> Component type
+ */
+interface HasDaggerComponent<T> {
+
+    /**
+     * Returns key string which will be used to store the component
+     *
+     * @return Key value
+     */
+    fun getComponentKey(): String
+
+    /**
+     * Returns actual component which will be stored
+     *
+     * @return Initialised component
+     */
+    fun getComponent(): T
+}
