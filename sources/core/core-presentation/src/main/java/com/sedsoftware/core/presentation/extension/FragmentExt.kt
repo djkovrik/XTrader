8bbc90ea3ae6539e39b0f88203efad76c96bcd76
DJ_Kovrik <djkovrik@gmail.com>
2020-03-31 19:29:23 +0300

    More renaming

--- sources/core/core-presentation/src/main/java/com/sedsoftware/core/presentation/extension/FragmentExt.kt
+++ sources/core/core-presentation/src/main/java/com/sedsoftware/core/presentation/extension/FragmentExt.kt
@@ -4,7 +4,6 @@ import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.sedsoftware.core.utils.extension.orZero
 
 fun Fragment.string(@StringRes resId: Int, vararg formatArgs: Any? = arrayOfNulls(0)): String =
     context?.getString(resId, formatArgs).orEmpty()
