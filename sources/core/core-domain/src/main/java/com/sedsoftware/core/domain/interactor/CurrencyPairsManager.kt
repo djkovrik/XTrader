d15daf94487858355fe2be62989883c56f40a88b
DJ_Kovrik <djkovrik@gmail.com>
2020-05-06 20:11:46 +0300

    Pairs manager redo

--- sources/core/core-domain/src/main/java/com/sedsoftware/core/domain/interactor/CurrencyPairsManager.kt
+++ sources/core/core-domain/src/main/java/com/sedsoftware/core/domain/interactor/CurrencyPairsManager.kt
@@ -3,34 +3,26 @@ package com.sedsoftware.core.domain.interactor
 import com.sedsoftware.core.domain.entity.Currency
 import com.sedsoftware.core.domain.repository.PairsManagerRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
 
 interface CurrencyPairsManager {
 
     val repository: PairsManagerRepository
 
-    suspend fun checkIfDataAvailable(): Flow<Boolean> = flow {
-        emit(repository.isSynchronized())
-    }.flowOn(Dispatchers.IO)
+    suspend fun checkIfDataAvailable(): Boolean = withContext(Dispatchers.IO) {
+        repository.isSynchronized()
+    }
 
-    suspend fun getBaseCurrencies(): Flow<List<Currency>> = flow {
-        val currencies =
-            repository
-                .getBaseCurrencies()
-                .sortedBy { it.name }
-                .distinctBy { it.name }
+    suspend fun getBaseCurrencies(): List<Currency> = withContext(Dispatchers.IO) {
+        repository
+            .getBaseCurrencies()
+            .sortedBy { it.name }
+            .distinctBy { it.name }
+    }
 
-        emit(currencies)
-    }.flowOn(Dispatchers.IO)
-
-    suspend fun getMarketCurrencies(base: Currency): Flow<List<Currency>> = flow {
-        val currencies =
-            repository
-                .getMarketCurrencies(base)
-                .sortedBy { it.name }
-
-        emit(currencies)
-    }.flowOn(Dispatchers.IO)
+    suspend fun getMarketCurrencies(base: Currency): List<Currency> = withContext(Dispatchers.IO) {
+        repository
+            .getMarketCurrencies(base)
+            .sortedBy { it.name }
+    }
 }
